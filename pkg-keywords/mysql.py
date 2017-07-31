c6529e11a99166ca0209a4ed19e07d4be6d5b4d3
jasonxia23 <xia.jason23@gmail.com>
2017-07-31 19:35:00 +0800

    feat keywords stats

---
+++ pkg-keywords/mysql.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import MySQLdb
+
+
+def increase(keyword):
+    db = MySQLdb.connect("localhost", "root", "", "registry")
+    cursor = db.cursor()
+    try:
+        cursor.execute("INSERT INTO keywords VALUES ('%s', 1)\
+            ON DUPLICATE KEY UPDATE count=count+1;" % (keyword))
+        db.commit()
+    except Exception as e:
+        db.rollback()
+    db.close()
