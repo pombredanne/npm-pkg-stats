f9d3c15007f6fd816c87863d271a4fb5131fc0c9
jasonxia23 <xia.jason23@gmail.com>
2017-10-15 03:13:15 +0800

    func: pkg-prefix

---
+++ pkg-prefix/prefix.py
@@ -0,0 +1,19 @@
+import couchdb
+import time
+import mysql
+
+couch = couchdb.Server()
+db = couch['registry']
+start = time.clock()
+
+for pkg in db:
+    print pkg
+    
+    # try:
+    #     keywords = db[pkg]['keywords']
+    #     for keyword in keywords:
+    #         mysql.increase(keyword)
+    # except:
+    #     continue
+
+print time.clock() - start
